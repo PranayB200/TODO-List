058c6a3ae21618216fb0db696566ddda4d0d95d6
Pranay Bansal <44213415+PranayB200@users.noreply.github.com>
2021-01-31 16:33:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+let ulTasks = $('#ulTasks')
+let btnAdd = $('#btnAdd')
+let btnReset = $('#btnReset')
+let inpNewTask = $('#inpNewTask')
+let btnCleanup = $('#btnCleanup')
+let btnSort = $('#btnSort')
+
+function addItem() {
+    let listItem = $('<li>', {
+        'class': 'list-group-item',
+        text: inpNewTask.val()
+    }) 
+
+    // Disabling the listItem on-click
+    listItem.click((event) => {
+        $(event.target).toggleClass('done')
+    })
+
+    ulTasks.append(listItem)
+    inpNewTask.val("")
+
+    toggleInputButtons()
+}
+
+function clearDone() {
+    $('#ulTasks .done').remove();
+    toggleInputButtons()
+}
+
+function sortTasks() {
+    $('#ulTasks .done').appendTo(ulTasks);
+    toggleInputButtons()
+}
+
+function toggleInputButtons() {
+    btnReset.prop('disabled', inpNewTask.val() == '')
+    btnAdd.prop('disabled', inpNewTask.val() == '')
+    btnSort.prop('disabled', ulTasks.children().length < 1)
+    btnCleanup.prop('disabled', ulTasks.children().length < 1)
+}
+
+inpNewTask.on('input', () => {
+    toggleInputButtons()
+})
+
+
+
+inpNewTask.keypress((e) => {
+    // 13 is for "Enter" button.
+    if (e.which == 13) {
+        addItem();
+    }
+})
+
+
+btnAdd.click(() => {
+    addItem();
+})
+
+btnReset.click(() => {
+    inpNewTask.val("")
+    toggleInputButtons()
+})
+
+btnCleanup.click(() => {
+    clearDone();
+})
+
+btnSort.click(() => {
+    sortTasks();
+})
